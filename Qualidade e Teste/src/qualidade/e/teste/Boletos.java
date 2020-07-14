521507eb836e127fc1006f221a530ffdafb40241
Daniel Salles <danielsalles@id.uff.br>
2020-07-13 21:44:23 -0300

    TDD12

--- Qualidade e Teste/src/qualidade/e/teste/Boletos.java
+++ Qualidade e Teste/src/qualidade/e/teste/Boletos.java
@@ -15,11 +15,14 @@ public class Boletos {
 }   
     public String quantosBoletosPagou(float p1, float p2, float p3, float div1){
         
-        if ((p1+p2+p2 >= div1) && ((p1 + p2 == 0)|| (p2+p3 == 0 ) || (p3+p1 ==0))){
+        if ((p1+p2+p3 >= div1) && ((p1 + p2 == 0)|| (p2+p3 == 0 ) || (p3+p1 ==0))){
             
             return "Pagou com 1 boleto";
-        }else{
+        }
+        if((p1+p2+p3 >= div1) && ((p1 == 0)|| (p2 == 0 ) || (p3 == 0))){
             return "Pagou com 2 boletos";
+        }else{
+            return "Pagou com 3 boletos";
         }
     }
     
